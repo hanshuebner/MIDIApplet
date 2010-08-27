8c7bc7024f68d97d87d18087b44b62168376f32e
Hans Huebner <hans.huebner@gmail.com>
2010-08-27 22:22:48 +0200

    first somewhat working version

--- mc-patchmanager.js
+++ mc-patchmanager.js
@@ -103,30 +103,6 @@ function uploadFirmware(hexData) {
     applet.send('f0 00 13 41 05 f7');
 }
 
-function getHexFile() {
-    $.get('mididata.hex', uploadFirmware);
-}
-
-function getPatches() {
-    $.get('/patches/', function (data) {
-        data = evalJSON(data);
-        if (!data.patches) {
-            addToLog('no patches returned from server');
-            return;
-        }
-        addToLog(data.patches.length + " patches received from server");
-        map(function (patch) {
-            $('#patches').append(
-                $(DIV({ class: 'patch' },
-                      map(function (fieldName) {
-                          return DIV({ class: fieldName }, patch[fieldName]);
-                      }, ['title', 'author', 'last-modified-date', 'device-id', 'comment']),
-                      DIV({ class: 'tags' },
-                          map(partial(DIV, null), patch.tags || [])))));
-        }, data.patches);
-    });
-}
-          
 function midiMessageReceived(message) {
     matchMidiMessage(message,
                      /^b. (..) (..)/, function (byte1, byte2) {
@@ -161,13 +137,142 @@ function midiMessageReceived(message) {
                      });
 }
 
+function getPatches() {
+    $.get('/patches/', function (data) {
+        data = evalJSON(data);
+        if (!data.patches) {
+            addToLog('no patches returned from server');
+            return;
+        }
+        addToLog(data.patches.length + " patches received from server");
+        var allTags = {};
+        map(function (patch) {
+            map(function (tag) {
+                allTags[tag]++;
+            }, patch.tags || []);
+            function displayField(name) {
+                return DIV({ class: name }, patch[name]);
+            }
+            var element = DIV({ class: 'patch' },
+                              map(displayField, ['title', 'author', 'last-modified-date']),
+                              DIV({ class: 'tags' },
+                                  map(partial(BUTTON, { class: 'tag' }), patch.tags || [])),
+                              displayField('comment'));
+            element.patch = patch;
+            $('#patches').append(
+                $(element)
+                    .mouseenter(function() {
+                        $(this).addClass('patchHighlight');
+                    })
+                    .mouseleave(function() {
+                        $(this).removeClass('patchHighlight');
+                    })
+                    .click(patchDetails));
+        }, data.patches);
+        map(function (tag) {
+            var element = BUTTON({ class: 'tag selectedTag' }, tag);
+            element.tag = tag;
+            $('#tags').append(
+                $(element)
+                    .click(function () {
+                        addToLog('tag ' + this.tag + ' clicked');
+                    }));
+        }, keys(allTags));
+    });
+}
+
+function uploadPatch()
+{
+    var patch = $(this).parent().parent().parent()[0].patch;
+    addToLog('uploading patchwith id ' + patch.id);
+    try {
+        uploadFirmware(patch['hex-data']);
+    }
+    catch (e) {
+        addToLog('error uploading firmware: ' + e);
+    }
+}
+
+function patchDetails(event)
+{
+    if (event) {
+        event.preventDefault();
+    }
+
+    $('#ajax-loader')
+        .css('top',  $(window).height() / 2 - $('#ajax-loader').height() / 2)
+        .css('left', $(window).width() / 2 - $('#ajax-loader').width() / 2);
+    $('#mask')
+        .click(function () {
+            $(this).fadeOut(200);
+            $('.modal-window').fadeOut(200);
+        })
+        .css({ 'width': $(window).width(),
+               'height': $(document).height()})
+        .fadeIn(200)
+        .fadeTo("slow", 0.8);
+
+    $.get('/patches/' + this.patch.id, function (data) {
+        var patch = evalJSON(data);
+
+        if (patch.documentation) {
+            $('#show-documentation')
+                .show()
+                .click(function () {
+                    $('#documentation')
+                        .empty()
+                        .append($(PRE(null, patch.documentation)));
+                    $('#documentation-window')
+                        .css('max-height', Math.round($(window).height() * 0.8))
+                        .css('top',  $(window).height() / 2 - $('#documentation-window').height() / 2)
+                        .css('left', $(window).width() / 2 - $('#documentation-window').width() / 2)
+                        .show();
+                });
+        } else {
+            $('#show-documentation')
+                .hide();
+        }
+        
+        function displayField(name) {
+            return DIV({ class: name },
+                       patch[name]);
+        }
+        $('#patch-details')
+            .empty()
+            .append($(DIV({ class: 'patch' },
+                          map(displayField, ['title', 'author', 'last-modified-date']),
+                          displayField('comment'))));
+        $('#upload-window')[0].patch = patch;
+        $('#upload-window')
+            .css('top',  $(window).height() / 2 - $('#upload-window').height() / 2)
+            .css('left', $(window).width() / 2 - $('#upload-window').width() / 2)
+            .fadeIn(200);
+    });
+}
+
 $(document).ready(function () {
     applet = document.applets[0];
 
     document.firmware = [];
 
     initMidiPortSelectors('inputSelect', 'outputSelect', applet, 'midiMessageReceived');
+
+    function maybeEnableUpload() {
+        $('#upload-button').attr('disabled',
+                                 ($('#inputSelect').val() && $('#outputSelect').val()) ? '' : 'disabled');
+    }
     
+    $('#inputSelect').change(maybeEnableUpload);
+    $('#outputSelect').change(maybeEnableUpload);
+    $('#upload-button').click(uploadPatch);
+
+    $('.close-window-icon').click(function () {
+        $(this).parent().fadeOut(100);
+        if ($(this).parent().attr('top')) {
+            $('#mask').fadeOut(100);
+        }
+    });
+
     restoreStateFromCookie();
 
     getPatches();
